5bb6c73d4c732c5fc3a01470143e979035e43374
Ankit Gadgil <agadgil@dhcp223-99.pnq.redhat.com>
2018-01-05 14:46:30 +0530

    added completre repo

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/geans.io/admin/');
+define('HTTP_CATALOG', 'http://localhost/geans.io/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/geans.io/admin/');
+define('HTTPS_CATALOG', 'http://localhost/geans.io/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/geans.io/admin/');
+define('DIR_SYSTEM', '/var/www/html/geans.io/system/');
+define('DIR_DATABASE', '/var/www/html/geans.io/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/geans.io/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/geans.io/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/geans.io/system/config/');
+define('DIR_IMAGE', '/var/www/html/geans.io/image/');
+define('DIR_CACHE', '/var/www/html/geans.io/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/geans.io/download/');
+define('DIR_LOGS', '/var/www/html/geans.io/system/logs/');
+define('DIR_CATALOG', '/var/www/html/geans.io/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'bike');
+define('DB_PASSWORD', 'bike123');
+define('DB_DATABASE', 'geans.io');
+define('DB_PREFIX', 'bs_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/geans.io/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/geans.io/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/geans.io/catalog/');
+define('DIR_SYSTEM', '/var/www/html/geans.io/system/');
+define('DIR_DATABASE', '/var/www/html/geans.io/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/geans.io/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/geans.io/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/geans.io/system/config/');
+define('DIR_IMAGE', '/var/www/html/geans.io/image/');
+define('DIR_CACHE', '/var/www/html/geans.io/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/geans.io/download/');
+define('DIR_LOGS', '/var/www/html/geans.io/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'bike');
+define('DB_PASSWORD', 'bike123');
+define('DB_DATABASE', 'geans.io');
+define('DB_PREFIX', 'bs_');
+?>
